0ebf3c366561f0e37f85af10deb7fcf06fb42cc9
DanFCo <danfcorreia@gmail.com>
2019-02-21 19:37:17 -0500

    apples and holidays

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,10 +17,14 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #     :memorial_day => ["BBQ"]
   #   }
   # }
+  holiday_hash[:summer][:fourth_of_july][1]
   # return the second element in the 4th of July array
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
+  holiday_hash[:winter][:christmas] << supply
+  holiday_hash[:winter][:new_years] << supply
+
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
@@ -29,23 +33,42 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
+  holiday_hash[:spring][:memorial_day] << supply
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
+  holiday_hash[season] = {holiday_name => supply_array}
+# code here
   # remember to return the updated hash
+  #holiday_hash
 
 end
 
 def all_winter_holiday_supplies(holiday_hash)
+
+holiday_hash[:winter].values.flatten
   # return an array of all of the supplies that are used in the winter season
 
 end
 
 def all_supplies_in_holidays(holiday_hash)
+     to_capitalize = []
+
+  holiday_hash.each do |season,value|
+  puts "#{season.to_s.capitalize!}"+ ":"
+
+
+      value.each do |holiday,supplies|
+
+         puts "  #{holiday.to_s.split("_").map(&:capitalize).join(' ')}: #{supplies.join(", ")}"
+  end
+    end
+
+
+
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
@@ -56,15 +79,20 @@ def all_supplies_in_holidays(holiday_hash)
 
 end
 
+
 def all_holidays_with_bbq(holiday_hash)
+     bbq_holidays =[]
+  holiday_hash.each do |season,holiday|
+      holiday.each do |holiday,supply|
+         if supply.include?("BBQ")
+        bbq_holidays << holiday
+
+
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
-end
-
-
-
-
-
-
-
+  end
+    end
+      end
+   bbq_holidays
+     end
